495d4f1a2108265aebd14fc02a2e551c276c7a00
tjamesmac <tjamesmac@gmail.com>
2019-11-21 17:31:25 +0000

    Format date

--- server/index.js
+++ server/index.js
@@ -6,7 +6,10 @@ const movies = require('../movies.json');
 const helpers = require('./helpers');
 
 
-helpers.formatDate();
+const testDate = helpers.formatDate(1294012800);
+console.log(testDate);
+
+
 
 app.get('/', (req, res) => res.send('Hello world'));
 
@@ -23,6 +26,11 @@ app.get('/movies/:id', (req, res) => {
   const { id } = req.params;
   const movieID = id - 1;
   if (movieID >= 0) {
+    const targetMovie = movies.movies[movieID];
+    let targetMovieComments = movies.movies[0].comments;
+    for (const comment of targetMovieComments) {
+    comment.dateCreated = helpers.formatDate(parseInt(comment.dateCreated));
+    }
     res.send(movies.movies[movieID]);
   } else {
     res.send({ "error": "No movie found"})
