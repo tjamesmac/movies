85314091d8a6ddc51b0b7bfdd89cb9bda226d307
tjamesmac <tjamesmac@gmail.com>
2019-11-21 18:23:12 +0000

    get most comments

--- server/helpers.js
+++ server/helpers.js
@@ -7,7 +7,6 @@ const self = module.exports = {
     let count = 0;
     for (const item of movies) {
       if (item.available_in_3d) {
-        console.log(item);
         ++count
       }
     }
@@ -15,15 +14,36 @@ const self = module.exports = {
     return count;
   },
   getMostUserComments: (movies) => {
-
+    const userCommentCount = {};
+    for (const item of movies) {
+      for (const comment of item.comments) {
+        const userName = comment.user;
+        if (userCommentCount.hasOwnProperty(userName)) {
+          userCommentCount[userName] = userCommentCount[userName] + 1;
+        } else {
+          userCommentCount[userName] = 1;
+        }
+      }
+    }
+    const mostComments = Object.keys(userCommentCount).reduce((a, b) => {
+      return userCommentCount[a] > userCommentCount[b] ? a : b;
+    })
+    console.log(mostComments);
+    return mostComments;
   },
   getHighestOscarsNoms: (movies) => {
-
+    const oscarNominations = movies.map((movie, index) => {
+      return movie.oscar_nominations;
+    })
+    console.log(Math.max(...oscarNominations));
+    return Math.max(...oscarNominations);
   },
   getRecentComments: (movies) => {
 
   },
   createReport: (movies) => {
-    const threeDeeMovies = self.get3DMovies(movies)
+    const threeDeeMovies = self.get3DMovies(movies);
+    const oscarNominations = self.getHighestOscarsNoms(movies);
+    const userComments = self.getMostUserComments(movies);
   }
 }
