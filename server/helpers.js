1c9a2c6620248ed883a4851e9b5c1f5fcfa1b442
tjamesmac <tjamesmac@gmail.com>
2019-11-21 19:10:05 +0000

    Finish report and comments

--- server/helpers.js
+++ server/helpers.js
@@ -39,11 +39,33 @@ const self = module.exports = {
     return Math.max(...oscarNominations);
   },
   getRecentComments: (movies) => {
-
+    const comments = [];
+    for (const item of movies) {
+      for (const comment of item.comments) {
+        comments.push(comment);
+      }
+    }
+    const recentComments = comments
+      .sort((a, b) => new Date(parseInt(b.dateCreated)) - new Date(parseInt(a.dateCreated)))
+      .filter((item, index) => index < 5);
+    const formatCommentDate = recentComments.map((comment) => {
+      comment.dateCreated = self.formatDate(parseInt(comment.dateCreated));
+      return comment;
+    })
+    console.log(formatCommentDate);
+    return formatCommentDate;
   },
   createReport: (movies) => {
     const threeDeeMovies = self.get3DMovies(movies);
-    const oscarNominations = self.getHighestOscarsNoms(movies);
     const userComments = self.getMostUserComments(movies);
+    const oscarNominations = self.getHighestOscarsNoms(movies);
+    const recentComments = self.getRecentComments(movies);
+    const report = {
+      movies_available_in_3d: threeDeeMovies,
+      user_with_most_comments: userComments,
+      most_oscar_nominations: oscarNominations,
+      newest_comments: recentComments,
+    }
+    return report;
   }
 }
