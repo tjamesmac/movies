de3874bbdaffc5a101e1b5164a300d0f6f2105f5
tjamesmac <tjamesmac@gmail.com>
2019-11-21 17:48:46 +0000

    get3D movies

--- server/helpers.js
+++ server/helpers.js
@@ -1,6 +1,29 @@
-module.exports = {
+const self = module.exports = {
   formatDate: (date) => {
     const formatDate = new Date(date * 1000).toISOString().split('T')[0];
     return formatDate; 
+  },
+  get3DMovies: (movies) => {
+    let count = 0;
+    for (const item of movies) {
+      if (item.available_in_3d) {
+        console.log(item);
+        ++count
+      }
+    }
+    console.log(count);
+    return count;
+  },
+  getMostUserComments: (movies) => {
+
+  },
+  getHighestOscarsNoms: (movies) => {
+
+  },
+  getRecentComments: (movies) => {
+
+  },
+  createReport: (movies) => {
+    const threeDeeMovies = self.get3DMovies(movies)
   }
 }
