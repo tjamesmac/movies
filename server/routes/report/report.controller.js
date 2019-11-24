ff57c38ed8923e87f2649e179d6d35f1b7f3a69c
tjamesmac <tjamesmac@gmail.com>
2019-11-24 12:45:57 +0000

    Create new object

--- server/routes/report/report.controller.js
+++ server/routes/report/report.controller.js
@@ -1,9 +1,10 @@
-const movies = require('../../../movies.json');
+const moviesJSON = require('../../../movies.json');
 const helpers = require('../../helpers');
 
 const controller = module.exports = {
   getReport: (req, res) => {
-    const report = helpers.createReport(movies.movies);
+    const movies = JSON.parse(JSON.stringify(moviesJSON.movies));
+    const report = helpers.createReport(movies);
     res.send(report);
   },
 };
