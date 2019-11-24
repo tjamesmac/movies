1a26a750eca0d8af869659acaa311eb3c06be4aa
tjamesmac <tjamesmac@gmail.com>
2019-11-24 12:55:08 +0000

    Format all movie comment dates

--- server/routes/movies/movies.controller.js
+++ server/routes/movies/movies.controller.js
@@ -1,7 +1,16 @@
-const movies = require('../../../movies.json');
+const moviesJSON = require('../../../movies.json');
+const helpers = require('../../helpers');
 
 const controller = module.exports = {
   getMovies: (req, res) => {
+    const movies = JSON.parse(JSON.stringify(moviesJSON.movies));
+    const formatDates = movies.map((item, index) => {
+      const comments = item.comments.map((comment, index) => {
+        const getDate = helpers.formatDate(parseInt(comment.dateCreated));
+        comment.dateCreated = getDate;
+        return comment;
+      });
+    })
     res.send(movies);
   },
 };
